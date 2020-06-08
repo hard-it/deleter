81fe874c763bf07caa667f4ca24de20b4f07fef0
Nikolay Lamanov <59505751+hard-it@users.noreply.github.com>
2020-06-08 15:57:34 +0300

    Script deleting files on ftp

---
+++ deleter.php
@@ -0,0 +1,23 @@
+<?
+$deldir = "bitrix";
+rmdirr($deldir);
+
+function rmdirr($dirname)
+{
+    if (!file_exists($dirname)) {
+        return false;
+    }
+    if (is_file($dirname)) {
+        return unlink($dirname);
+    }
+    $dir = dir($dirname);
+    while (false !== $entry = $dir->read()) {
+        if ($entry == '.' || $entry == '..') {
+            continue;
+        }
+        rmdirr("$dirname/$entry");
+    }
+    $dir->close();
+    return rmdir($dirname);
+}
+?>
